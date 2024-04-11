940c01649a2e09a49bdbe2bcae32c610c713b4c5
Arjon200141 <aorjon123@gmail.com>
2024-04-12 00:11:45 +0600

    json Added

--- src/Shared/LeftSideNav.jsx
+++ src/Shared/LeftSideNav.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+    const [ categories, setCategories ] = useState([]);
+
+    useEffect(() => {
+        fetch("categories.json")
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
+
     return (
-        <div>
-            <h3>Left Side Nav</h3>
+        <div className="space-y-4">
+            <h3 className="text-xl font-medium">All Categories</h3>
+            {
+                categories.map(category => <Link className=" px-8 block text-xl font-medium" key={category.id} to = {`/category/${category.id}`} >{category.name} </Link>)
+            }
+            
+
         </div>
     );
 };
